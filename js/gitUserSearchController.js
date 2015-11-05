e3f341c7aff81a36575901f13e97e8485565fa83
Gustaf Rudholm <gustafrudholm@Gustafs-MacBook-Pro.local>
2015-11-05 10:51:33 +0100

    required searchfactory file in index.html

--- js/gitUserSearchController.js
+++ js/gitUserSearchController.js
@@ -1,4 +1,4 @@
-githubUserSearch.controller('GitUserSearchController', ['$resource', function($resource, $searchFactory) {
+githubUserSearch.controller('GitUserSearchController', ['Search', function(Search) {
 
         var self = this;
 
